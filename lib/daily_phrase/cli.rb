c6f8b85dd408e9e0209e7f59936a9d0ead1b04d8
Jennifer Dobak <jdobak1@gmail.com>
2018-02-09 22:50:47 +0000

    adjusted user prompts

--- lib/daily_phrase/cli.rb
+++ lib/daily_phrase/cli.rb
@@ -1,7 +1,7 @@
 class DailyPhrase::CLI
 
   def call
-    puts "Welcome to globoyak's Daily Phrases!"
+    puts "***Welcome to globoyak's Daily Language Phrases!!!***"
     menu
     # list_phrases
 
@@ -16,20 +16,32 @@ class DailyPhrase::CLI
   # end
 
   def menu #currently unable to handle multiple language input that is not separated by a comma
-    puts "Please indicate which languages you would like to see, or enter 'all'"
+    puts <<-DOC.gsub /^\s+/, ""
+
+    Please type in the languages for which you would like to see phrases.
+    ~To see a complete list of available languages, type 'list'.
+    ~If you would like to phrases in all 15 languages, type 'all'.
+    ~To exit the program, type 'quit'.
+    DOC
+
     input = gets.strip
     array = input.split(", ")
+
     if input == "all"
       puts "Here's what people around the world are saying today:"
       puts "All 15 language phrases"
+    if input == "list"
+      list
+    elsif input == "quit"
+      exit
     elsif array.length == 1
       puts "Let's see what people are saying in #{input.capitalize} today:"
       input.split(", ").each{|language| puts "Phrase in #{language.capitalize}"}
-      else
-        new_array = array.collect{|language| language.capitalize}
-        puts "Let's see what people are saying in #{new_array[0...new_array.length-1].join(", ")} and #{new_array[new_array.length-1]} today:"
-        input.split(", ").each{|language| puts "Phrase in #{language.capitalize}"}
-        end
+    else
+      new_array = array.collect{|language| language.capitalize}
+      puts "Let's see what people are saying in #{new_array[0...new_array.length-1].join(", ")} and #{new_array[new_array.length-1]} today:"
+      input.split(", ").each{|language| puts "Phrase in #{language.capitalize}"}
+    end
   end
 
 end
