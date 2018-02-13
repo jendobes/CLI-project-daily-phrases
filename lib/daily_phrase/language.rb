c3b872b36f0a600ae57e15f7b6416df65ea9cefa
Jennifer Dobak <jdobak1@gmail.com>
2018-02-13 15:56:16 +0000

    began scraper method for Language greeting

--- lib/daily_phrase/language.rb
+++ lib/daily_phrase/language.rb
@@ -1,4 +1,11 @@
 class DailyPhrase::Language
+  def self.scraper(input)
+      greetings = []
+      doc = Nokogiri::HTML(open("http://pocketcultures.com/2008/10/30/say-hello-in-20-languages/"))
+      # doc.search("p strong").each {|language| greetings << language}
+      #   doc.search("p strong").text.split(/(\d.)/)
 
+  end
+  
 
 end
