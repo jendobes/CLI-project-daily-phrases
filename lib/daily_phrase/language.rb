603e7d3616d16f1564f2098f997698b178620891
Jennifer Dobak <jdobak1@gmail.com>
2018-02-13 16:54:34 +0000

    Language objects have attributes

--- lib/daily_phrase/language.rb
+++ lib/daily_phrase/language.rb
@@ -1,4 +1,18 @@
 class DailyPhrase::Language
+attr_accessor :name, :greeting, :phrase, :translation
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
   def self.scraper(input)
       greetings = []
       doc = Nokogiri::HTML(open("http://pocketcultures.com/2008/10/30/say-hello-in-20-languages/"))
@@ -6,6 +20,6 @@ class DailyPhrase::Language
       #   doc.search("p strong").text.split(/(\d.)/)
 
   end
-  
+
 
 end
