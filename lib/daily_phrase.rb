0ba53d8561ee58be95af75a2dd8a62979790e259
Jennifer Dobak <jdobak1@gmail.com>
2018-02-15 18:58:26 +0000

    renamed phrases.rb to scrape.rb

--- lib/daily_phrase.rb
+++ lib/daily_phrase.rb
@@ -1,6 +1,6 @@
 require "./lib/daily_phrase/version"
 require "./lib/daily_phrase/cli"
-require "./lib/daily_phrase/phrases"
+require "./lib/daily_phrase/scrape"
 require "./lib/daily_phrase/language"
 require "nokogiri"
 require "open-uri"
